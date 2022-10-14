6ccaa748a50e645c9c3276d154c937f6de89b0f2
icyhot12 <rmierniczek1@gmail.com>
2022-10-14 10:51:48 +0200

    form test code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react"
 
 function App() {
+
+  const [form, setForm] = useState({
+    name: "",
+    lastname: "",
+    email: "",
+    color: "",
+    like: false
+  })
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    console.log(form)
+  }
+
+  function handleChange(event) {
+    const {name, value, type, checked} = event.target
+    setForm(prevForm => {
+      return {
+        ...prevForm,
+        [name]: type === "checkbox" ? checked : value
+      }
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="name" name="name" onChange={handleChange} value={form.name} />
+      <input type="text" placeholder="lastname" name="lastname" onChange={handleChange} value={form.lastname} />
+      <input type="email" placeholder="example@example.com" name="email" onChange={handleChange} value={form.email} />
+      <label>
+        Pick your favorite flavor:
+        <select onChange={handleChange} name="color" value={form.color}>
+          <option value=""> -- Pick flavor -- </option>
+          <option value="grapefruit">Grapefruit</option>
+          <option value="lime">Lime</option>
+          <option value="coconut">Coconut</option>
+          <option value="mango">Mango</option>
+        </select>
+      </label>
+      <label>
+        <input type="checkbox" name="like" checked={form.like} onChange={handleChange}/>
+        Do you like me?
+      </label>
+      <button>Submit</button>
+    </form>
   );
 }
 
